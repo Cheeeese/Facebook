905661e1d9e1949d1238d6c2fe4083f00ac5034a
Matt Verghese <matt.verghese@gmail.com>
2016-03-05 16:53:26 -0800

    Initial Commit
    Initial Project Commit

---
+++ Facebook/NewsFeedViewController.swift
@@ -0,0 +1,55 @@
+//
+//  NewsFeedViewController.swift
+//  Facebook
+//
+//  Created by Timothy Lee on 8/3/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var feedImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Configure the content size of the scroll view
+        scrollView.contentSize = CGSizeMake(320, feedImageView.image!.size.height)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        scrollView.contentInset.top = 0
+        scrollView.contentInset.bottom = 50
+        scrollView.scrollIndicatorInsets.top = 0
+        scrollView.scrollIndicatorInsets.bottom = 50
+    }
+    
+    
+    @IBAction func onTapGesturePhoto(sender: UITapGestureRecognizer) {
+        performSegueWithIdentifier("expandPhotoSegue", sender: self)
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        let destinationViewController = segue.destinationViewController as! PhotoViewController
+        
+        
+        
+
+    }
+
+    
+    
+    
+}
---
+++ Facebook/PhotoViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PhotoViewController.swift
+//  Facebook
+//
+//  Created by Matthew Verghese on 3/5/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+    
+    @IBOutlet weak var photoImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onTapDone(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
